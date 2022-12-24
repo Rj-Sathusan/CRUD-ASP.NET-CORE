ca1660353233fd27eb602f2c3391a6aacd2a2651
Rj-Sathusan <www.rjsathusan@gmail.com>
2022-12-24 21:46:19 +0530

    first commit

---
+++ Models/user.cs
@@ -0,0 +1,14 @@
+﻿namespace TRY.Models
+{
+    public class user
+    {
+        public string? Name { get; set; }
+        public int id { get; set; }
+
+        public string? Address { get; set; }
+        public user()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TRY;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebApi.Helpers.ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase") ?? throw new InvalidOperationException("Connection string 'WebApiDatabase' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
